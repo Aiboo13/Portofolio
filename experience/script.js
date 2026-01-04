577b50fad5f9324b13e653a7866de518c7fcb40e
Aiboo13 <minasssahiddin@gmail.com>
2026-01-04 21:22:17 +0700

    modif ./projects/

--- experience/script.js
+++ experience/script.js
@@ -65,7 +65,7 @@ document.onkeydown = function(e) {
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Minas Sahiddin";
+        document.title = "Experience | Portfolio Jigar Sable";
         $("#favicon").attr("href","/assets/images/favicon.png");
     }
     else {
